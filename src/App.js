1a194c4c4b66bbdea78aa7f22fc30ad9360b7538
Meher Atharva <meheratharva49@gmail.com>
2022-09-23 19:41:24 +0530

    warnings fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import { Navbar } from 'react-bootstrap';
 import Navbar1 from '../src/components/navbar';
 
 function App() {
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -25,13 +25,13 @@ import "../components/navbar.css";
 export default function navbar() {
   return (
     <div>
-      <img alt="imageq"  className="leftshape" src={leftshape} />
-      <img alt="image2"  className="people" src={people} />
+      <img alt="mageq"  className="leftshape" src={leftshape} />
+      <img alt="mage2"  className="people" src={people} />
       <div>
         <Navbar bg="light" expand="lg" id="navbar-main">
           <Container>
             <Navbar.Brand id="navbar-logo">
-              <img alt="image3"  src={logo} />
+              <img alt="mage3"  src={logo} />
             </Navbar.Brand>
 
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -79,22 +79,22 @@ export default function navbar() {
         </div>
 
         <div className="how-can">
-          <img alt="image4"  className="how-can-image1" src={Trainings} />
-          <img alt="image5"   className="how-can-image" src={Webdev} />
+          <img alt="mage4"  className="how-can-image1" src={Trainings} />
+          <img alt="mage5"   className="how-can-image" src={Webdev} />
 
-          <img alt="image6"  className="how-can-image" src={Softdev} />
+          <img alt="mage6"  className="how-can-image" src={Softdev} />
 
-          <img alt="image7"  className="how-can-image" src={Appdev} />
+          <img alt="mage7"  className="how-can-image" src={Appdev} />
         </div>
       </div>
       <div className="container-intersect" >
         <div className="intersect-div">
-          <img alt="image8"  className="Intersect" src={Intersect} />
-          <img alt="image9"  className="Ellpise2" src={Ellipse2} />
+          <img alt="mage8"  className="Intersect" src={Intersect} />
+          <img alt="mage9"  className="Ellpise2" src={Ellipse2} />
         </div>
         <div className="our-work">
           <div className="work-sections">
-            <img alt="imagea"  className="Group1" src={Group1} />
+            <img alt="magea"  className="Group1" src={Group1} />
             <span className="Group-spans">
               Data Analysis for Dissertation / Thesis of Ph.D
             </span>
@@ -104,11 +104,11 @@ export default function navbar() {
             <span className="Group-spans">
               Assignments / Project for IT-Comp. Science
             </span>
-            <img alt="imaged"  className="Group1" src={Group2} />
+            <img alt="maged"  className="Group1" src={Group2} />
           </div>
 
           <div className="work-sections">
-          <img alt="imagef"  className="Group1" src={Group3} />
+          <img alt="magef"  className="Group1" src={Group3} />
             <span className="Group-spans">
             Technical Writing Service - Manual / User Guide preparation            </span>
           </div>
@@ -117,11 +117,11 @@ export default function navbar() {
             <span className="Group-spans">
             Video Creation            
             </span>
-            <img alt="imageg"  className="Group1" src={Group4} />
+            <img alt="mageg"  className="Group1" src={Group4} />
           </div>
 
           <div className="work-sections">
-          <img alt="imageh"  className="Group1" src={Group5} />
+          <img alt="mageh"  className="Group1" src={Group5} />
             <span className="Group-spans">
             Academics Service - Book chapters writing / QB development            </span>
           </div>
