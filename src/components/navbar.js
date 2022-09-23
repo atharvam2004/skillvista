16e4ba1f64980d03af578a9b882924a5b8c893e7
Meher Atharva <meheratharva49@gmail.com>
2022-09-23 19:35:04 +0530

    wa=ning fixed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,6 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import logo from "../images/logo.png";
 import leftshape from "../images/leftshape.png";
 import people from "../images/people-group.png";
@@ -12,23 +11,27 @@ import Softdev from "../images/Softdev.png";
 import Webdev from "../images/Webdev.png";
 import Trainings from "../images/Trainings.png";
 
-
 import Ellipse2 from "../images/Ellipse2.png";
 import Intersect from "../images/Intersect.png";
 
+import Group1 from "../images/Group1.png";
+import Group2 from "../images/Group2.png";
+import Group3 from "../images/Group3.png";
+import Group4 from "../images/Group4.png";
+import Group5 from "../images/Group5.png";
 
 import "../components/navbar.css";
 
 export default function navbar() {
   return (
     <div>
-            <img className="leftshape" src={leftshape} />
-      <img className="people" src={people} />
+      <img alt="imageq"  className="leftshape" src={leftshape} />
+      <img alt="image2"  className="people" src={people} />
       <div>
         <Navbar bg="light" expand="lg" id="navbar-main">
           <Container>
             <Navbar.Brand id="navbar-logo">
-              <img src={logo} />
+              <img alt="image3"  src={logo} />
             </Navbar.Brand>
 
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -52,42 +55,77 @@ export default function navbar() {
         </Navbar>
       </div>
 
-
-
       <div className="Home-titles">
         <h5 id="welcome">WELCOME</h5>
 
-        <h3 id="digital">Digital Products  <br/>For your Ideas</h3>
+        <h3 id="digital">
+          Digital Products <br />
+          For your Ideas
+        </h3>
 
         <div id="ensuring">
-          <span>Ensuring the best</span> <br/>
+          <span>Ensuring the best</span> <br />
           <span>returns on your requirements...</span>
         </div>
       </div>
 
       <div className="how-can-we">
         <div id="how-can-text">
-          <h4 >How can we <br/>help you?</h4>
-        <hr id="help-hr"></hr></div>
-        
-        <div className="how-can">
-        <img className="how-can-image" src={Trainings} />
-        <img className="how-can-image" src={Webdev} />
-
-        <img className="how-can-image" src={Softdev} />
-
-        <img className="how-can-image" src={Appdev} />
+          <h4>
+            How can we <br />
+            help you?
+          </h4>
+          <hr id="help-hr"></hr>
         </div>
-        
+
+        <div className="how-can">
+          <img alt="image4"  className="how-can-image1" src={Trainings} />
+          <img alt="image5"   className="how-can-image" src={Webdev} />
+
+          <img alt="image6"  className="how-can-image" src={Softdev} />
+
+          <img alt="image7"  className="how-can-image" src={Appdev} />
+        </div>
       </div>
+      <div className="container-intersect" >
+        <div className="intersect-div">
+          <img alt="image8"  className="Intersect" src={Intersect} />
+          <img alt="image9"  className="Ellpise2" src={Ellipse2} />
+        </div>
+        <div className="our-work">
+          <div className="work-sections">
+            <img alt="imagea"  className="Group1" src={Group1} />
+            <span className="Group-spans">
+              Data Analysis for Dissertation / Thesis of Ph.D
+            </span>
+          </div>
 
+          <div className="work-sections">
+            <span className="Group-spans">
+              Assignments / Project for IT-Comp. Science
+            </span>
+            <img alt="imaged"  className="Group1" src={Group2} />
+          </div>
 
-      <div className="intersect-div">
-      <img className="Intersect" src={Intersect} />
-      <img className="Ellpise2" src={Ellipse2} />
+          <div className="work-sections">
+          <img alt="imagef"  className="Group1" src={Group3} />
+            <span className="Group-spans">
+            Technical Writing Service - Manual / User Guide preparation            </span>
+          </div>
 
-       
+          <div className="work-sections">
+            <span className="Group-spans">
+            Video Creation            
+            </span>
+            <img alt="imageg"  className="Group1" src={Group4} />
+          </div>
 
+          <div className="work-sections">
+          <img alt="imageh"  className="Group1" src={Group5} />
+            <span className="Group-spans">
+            Academics Service - Book chapters writing / QB development            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
