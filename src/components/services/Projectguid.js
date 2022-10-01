c1b80b7c4805b92dd60e21ecc7c3b23c6f3719db
Meher Atharva <meheratharva49@gmail.com>
2022-10-02 00:21:26 +0530

    porject guide section 50% done

--- src/components/services/Projectguid.js
+++ src/components/services/Projectguid.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "../../Styles/services/Projectguid.css";
-
+import operation from "../../assets/img/operation.png";
+import supply from "../../assets/img/supply.png";
 function Projectguid() {
   return (
     <>
-    
+      <div className="Projectguide-main">
+        <div className="Projectguide-heading">
+          <h3>Project Guidance and Lectures</h3>
+        </div>
+        <div className="Projectguide-divs">
+          <div className="Projectguide-div">
+          <img alt="Data" className="Projectguideimg" src={operation} />
+          <div className="Projectguide-text"></div>
+
+          </div>
+          <div className="Projectguide-div">
+          <img alt="Data" className="Projectguideimg" src={supply} />
+          <div className="Projectguide-text"></div>
+
+
+          </div>
+        </div>
+      </div>
     </>
   );
 }
