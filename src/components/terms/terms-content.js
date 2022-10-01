27cfd40d83e4f7bdce0cdd66ccd5ca8fcbe24e35
Meher Atharva <meheratharva49@gmail.com>
2022-10-01 19:55:22 +0530

    minor

--- src/components/terms/terms-content.js
+++ src/components/terms/terms-content.js
@@ -4,26 +4,20 @@ import "../../Styles/terms/terms.css";
 function termscontent() {
   return (
     <>
-  
-      
       <div className="terms-content">
         <p>Last Updated on: [enter date]</p>
-
         <p>
           By using this website as a user (&quot;You&quot;), You agree to the
           following Terms and Conditions and Privacy Policy. Please read them
           carefully before using this website.
         </p>
-
         <b>General Provisions</b>
-
         <p>
           This website is owned and operated by [enter business name] (enser DBA
           name) (hereafter &quot;Our&quot;. &quot;We&quot; or
           &quot;Company&quot;). Our principal place of business is located at
           [enter address
         </p>
-
         <p>
           Use of this website is at Your own risk. We host this site on a
           reputable platform and take reasonable efforts to maintain and host
@@ -31,16 +25,12 @@ function termscontent() {
           as to the safety or Your individual use of the website. The terms and
           conditions contained on this page is subject to change at any time.
         </p>
-
         <b>Age Requirements</b>
-
         <p>
           You must be at least 18 to use Our website, templates, courses, and
           products
         </p>
-
         <b>Intellectual Property Notice</b>
-
         <p>
           All text, photographs graphics and other materials in this site are
           subject to the copyrights aist ather intellectual property rights of
@@ -53,7 +43,6 @@ function termscontent() {
           and/or an unction toning You to stop une Oue intellectual propony
           immediately
         </p>
-
         <p>Company on the following Trade) femerary apple team seal nimbers)</p>
       </div>
     </>
--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Headercontact from "../components/Contactus/Headercontact";
-
+import Navbar1 from "../components/navbar";
+import Footer from "../components/footer";
 
 function Contactus() {
   return (
-    <>
+    <>      <Navbar1 />
            <Headercontact />
+           <Footer />
+
 
 
     </>
